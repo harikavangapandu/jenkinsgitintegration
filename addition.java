f1997566af20affd0b31a78d5463e45c393ffbca
harikavangapandu <vangapanduharika.19.it@anits.edu.in>
2022-03-26 10:02:45 +0530

    sum of 2 numbers

---
+++ addition.java
@@ -0,0 +1,15 @@
+class Addition {
+
+  public static void main(String[] args) {
+    
+   
+    int first = 10;
+    int second = 20;
+    
+    System.out.println("Sum of two numbers:"+first + " " + second);
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println("The sum is: " + sum);
+  }
+}
